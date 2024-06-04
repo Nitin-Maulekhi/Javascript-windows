8acbe85b094b0ac870df6fe6f1653afffd4523bb
Nitin-Maulekhi <nitinmaulekhi68@gmail.com>
2024-06-05 05:18:36 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,59 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+//**************************Operations****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Hello"
+let str2 = " Nitin"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// Generally we don't write code in this way which we get problem to read the code.
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// // console.log(2 + 3 * 4 % 5);
+
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2 
+
+// console.log(num1, num2, num3);
+// console.table([num1, num2, num3])
+
+
+console.log(+true);
+console.log(+"");
+
+
+// Prefix Increment 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// Postfix Increment
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
