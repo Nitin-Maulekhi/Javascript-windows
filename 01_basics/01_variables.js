22901e7fb25fa2d805f76a2a465e82a1933c0f42
Nitin-Maulekhi <nitinmaulekhi68@gmail.com>
2024-06-04 21:24:04 +0000

    data variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,30 @@
+const accountId="1212111"
+let password="77873737837787"
+accountstate="Delhi"
+let clientemail="nitinmaulekhi68@gmail.com"
+let accountcity;
+var accountnumber = "112233445566778899"
+
+// console.log(accountId);
+// console.log(password);
+// console.log(accountstate);
+
+accountnumber="66554433"
+
+
+/*
+Prefer not to use var because of issues in block scope and functional scope
+*/
+
+// console.log(accountnumber);
+
+
+// accountId="7783927"//   not allowed to print this if the value comes in constant.
+
+password = "092998838"
+accountstate = "mumbai"
+clientemail = "maulekhi1008@gmail.com"
+
+// console.log(accountId);
+
+console.table([accountId, password, accountstate, clientemail, accountcity, accountnumber])
