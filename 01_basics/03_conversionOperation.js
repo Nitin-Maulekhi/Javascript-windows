ece6e52d9bf0a6da8aa4de988babe56594b5f607
Nitin-Maulekhi <nitinmaulekhi68@gmail.com>
2024-06-04 22:05:52 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Nitin"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not in Number)
+// true => 1
+// false => 0
+
+let isLoggedIn = "Nitin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// true => 1
+// false => 0
+// "" => false
+// "Nitin" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+// console.log(typeof stringNumber);
