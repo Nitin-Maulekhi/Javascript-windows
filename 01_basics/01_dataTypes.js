a0b41b19f1e85c7c6e9bf308fb91443e37376b04
Nitin-Maulekhi <nitinmaulekhi68@gmail.com>
2024-06-04 21:24:23 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as new version
+
+// alert(3 +3) // we are using nodejs, not browser so it will show error in nodejs but if we use this alert method in browser so it will work and show the answer.
+
+console.log(3 
+    +
+     3) // code readability should be high, so avoid to write your code like this and write in a proper way which others can read and understand the code easily.
+
+console.log("Nitin Maulekhi")
+
+
+let name = "Nitin"
+let age = 26
+let isLoggedIn = false
+let state = null
+
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean  => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object 
+
